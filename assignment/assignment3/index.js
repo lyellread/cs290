6bddcd1a4f93e5c23755234496dfdaad15454b4d
Lyell Read <lyellread@gmail.com>
2019-05-13 17:55:34 -0700

    Finishing assignment 3
    sorta a fabricobble, like my life ;)

--- assignment/assignment3/index.js
+++ assignment/assignment3/index.js
@@ -2,56 +2,54 @@
  * Add your JavaScript to this file to complete the assignment.
  */
 
-console.log("== Started.");
-var originalTwitList = document.getElementsByClassName('twit');
-var currentTwitExample = document.getElementsByClassName('twit')[0];
 
+/*
+ * Create Variables
+ */
+
+
+var removedTwits;
+var currentTwitExample = document.getElementsByClassName('twit')[0];
 var hiddenElements = document.getElementsByClassName('hidden');
 
+/*
+ * Add event listeners
+ */
 
 var createTwitButton = document.getElementById('create-twit-button');
-
 createTwitButton.addEventListener('click', function (event) {
-	console.log("==== Event Listener Triggered");
 	showNewTwitModal();
 });
 
 var modalAcceptButton = document.getElementsByClassName('modal-accept-button')[0];
-
 modalAcceptButton.addEventListener('click', function (event) {
-	console.log("==== Event Listener Triggered");
 	acceptModal();
 });
 
 var modalCancelButton = document.getElementsByClassName('modal-cancel-button')[0];
-
 modalCancelButton.addEventListener('click', function (event) {
 	hideNewTwitModal();
 });
 
 var modalCloseButton = document.getElementsByClassName('modal-close-button')[0];
-
 modalCloseButton.addEventListener('click', function (event) {
 	hideNewTwitModal();
 });
 
 var searchText = document.getElementById('navbar-search-input');
-
 searchText.addEventListener('input', function (event) {
-	
 	searchPage(searchText.value);
 	
 });
 
-
-
-
+/*
+ * Functions
+ */
 
 function showNewTwitModal(){
 	for (var i = 0; i < hiddenElements.length; i++){
 		hiddenElements[i].style.display = 'inline';
 	}
-	console.log("== Show Modal");
 }
 
 function hideNewTwitModal (){
@@ -61,9 +59,7 @@ function hideNewTwitModal (){
 	
 	for (var i = 0; i < hiddenElements.length; i++){
 		hiddenElements[i].style.display = 'none';
-	}
-	console.log("== Hide Modal");
-	
+	}	
 }
 
 function acceptModal(){
@@ -76,10 +72,9 @@ function acceptModal(){
 		return;
 	}
 	
-	console.log("== Collected new tweet info\n  == Content:" + newTwitContent + "\n  == Author:" + newTwitAuthor);
+	/*console.log("== Collected new tweet info\n  == Content:" + newTwitContent + "\n  == Author:" + newTwitAuthor);*/
 	
 	addNewTwit(newTwitContent, newTwitAuthor);
-	
 	hideNewTwitModal();
 }
 
@@ -97,77 +92,39 @@ function addNewTwit(content, author){
 	//console.log("== New Author Field:" +newTwit.children[1].children[1].children[0].textContent)
 
 	currentTwitContainer.appendChild(newTwit);
-	originalTwitList += newTwit;
-	
-}
 
-function emptyTwitContainer(){
-	
-	var container = document.getElementsByClassName('twit');
-	
-	for (var j = 0; j < container.length; j++){
-		container[j].remove();
-	}
-	
-	console.log("== Removed all from DOM");
 }
+	
 
 function searchPage (searchTerm) {
 	
-	var currentTwitContainer = document.getElementsByClassName('twit-container')[0];
-	
-	emptyTwitContainer();
-
-	console.log("Search Term:" + searchTerm + ":");
-	console.log("TwitList:");
-	console.log(originalTwitList);
-
 	if (searchTerm === ''){
 		
-		for (var i = 0; i < originalTwitList.length; i++){
-			currentTwitContainer.appendChild(originalTwitList[i]);
-		}
-		return;
+		location.reload();
 		
 	}
 	
-	for (var i = 0; i < originalTwitList.length; i++){
+	var currentTwitContainer = document.getElementsByClassName('twit-container')[0];
+
+	console.log("======== Search Term: ===========" + searchTerm + ":");
+	
+	var twitList = document.getElementsByClassName('twit');
+	
+	console.log("== Twit List:", twitList);
+	
+	for (var i = twitList.length - 1; i > -1; i--){
 		
+		console.log("== Checking twit:", twitList[i].textContent);
+		console.log("== Contains text:", twitList[i].textContent.search(searchTerm));		
 		
-		if (originalTwitList[i].children[1].children[0].textContent.search(searchTerm) !== -1 ||
-			originalTwitList[i].children[1].children[1].children[0].textContent.search(searchTerm) !== -1 ){
-				
-				console.log("Appending: " + i);
-				currentTwitContainer.appendChild(originalTwitList[i]);
-				
-			}
-	}
-}
+		if (twitList[i].textContent.search(searchTerm) === -1){
 			
-		
+			twitList[i].remove();
+			removedTwits += twitList[i];
+			
+		}
+	}
 	
+	console.log("== Twit List Post Search:", twitList);
 	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}

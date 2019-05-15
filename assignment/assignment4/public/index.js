358ef94b0d22a39fb7bddefc2ca3b0555f4ed91f
Lyell Read <lyellread@gmail.com>
2019-05-15 16:49:07 -0700

    Starter code + Transferred style.css from A3 to A4/public/

--- assignment/assignment4/public/index.js
+++ assignment/assignment4/public/index.js
@@ -1,8 +1,130 @@
 /*
- * Add the contents of your index.js file from Assignment 3 here to see the
- * interactions you implemented.  This is not required for your grade on this
- * assignment, but it'll allow you to have the full experience of the site
- * as we've implemented it so far.
+ * Add your JavaScript to this file to complete the assignment.
  */
 
-alert('JS successfully loaded.');
+
+/*
+ * Create Variables
+ */
+
+
+var removedTwits;
+var currentTwitExample = document.getElementsByClassName('twit')[0];
+var hiddenElements = document.getElementsByClassName('hidden');
+
+/*
+ * Add event listeners
+ */
+
+var createTwitButton = document.getElementById('create-twit-button');
+createTwitButton.addEventListener('click', function (event) {
+	showNewTwitModal();
+});
+
+var modalAcceptButton = document.getElementsByClassName('modal-accept-button')[0];
+modalAcceptButton.addEventListener('click', function (event) {
+	acceptModal();
+});
+
+var modalCancelButton = document.getElementsByClassName('modal-cancel-button')[0];
+modalCancelButton.addEventListener('click', function (event) {
+	hideNewTwitModal();
+});
+
+var modalCloseButton = document.getElementsByClassName('modal-close-button')[0];
+modalCloseButton.addEventListener('click', function (event) {
+	hideNewTwitModal();
+});
+
+var searchText = document.getElementById('navbar-search-input');
+searchText.addEventListener('input', function (event) {
+	searchPage(searchText.value);
+	
+});
+
+/*
+ * Functions
+ */
+
+function showNewTwitModal(){
+	for (var i = 0; i < hiddenElements.length; i++){
+		hiddenElements[i].style.display = 'inline';
+	}
+}
+
+function hideNewTwitModal (){
+	
+	newTwitContent = document.getElementById('twit-text-input').value = '';
+	newTwitAuthor = document.getElementById('twit-attribution-input').value = '';
+	
+	for (var i = 0; i < hiddenElements.length; i++){
+		hiddenElements[i].style.display = 'none';
+	}	
+}
+
+function acceptModal(){
+	
+	newTwitContent = document.getElementById('twit-text-input').value;
+	newTwitAuthor = document.getElementById('twit-attribution-input').value;
+	
+	if (newTwitContent === '' || newTwitAuthor === ''){
+		alert("Incomplete Field(s). Fix Before Submitting!")
+		return;
+	}
+	
+	/*console.log("== Collected new tweet info\n  == Content:" + newTwitContent + "\n  == Author:" + newTwitAuthor);*/
+	
+	addNewTwit(newTwitContent, newTwitAuthor);
+	hideNewTwitModal();
+}
+
+function addNewTwit(content, author){
+	
+	var currentTwitContainer = document.getElementsByClassName('twit-container')[0];
+	
+	var newTwit= currentTwitExample.cloneNode(true);
+	
+	//console.log("== New Author Attempt:" +newTwit.children[1].children[1].children[0])
+	
+	newTwit.children[1].children[0].textContent = content;
+	newTwit.children[1].children[1].children[0].textContent = author;
+	
+	//console.log("== New Author Field:" +newTwit.children[1].children[1].children[0].textContent)
+
+	currentTwitContainer.appendChild(newTwit);
+
+}
+	
+
+function searchPage (searchTerm) {
+	
+	if (searchTerm === ''){
+		
+		location.reload();
+		
+	}
+	
+	var currentTwitContainer = document.getElementsByClassName('twit-container')[0];
+
+	console.log("======== Search Term: ===========" + searchTerm + ":");
+	
+	var twitList = document.getElementsByClassName('twit');
+	
+	console.log("== Twit List:", twitList);
+	
+	for (var i = twitList.length - 1; i > -1; i--){
+		
+		console.log("== Checking twit:", twitList[i].textContent);
+		console.log("== Contains text:", twitList[i].textContent.search(searchTerm));		
+		
+		if (twitList[i].textContent.search(searchTerm) === -1){
+			
+			twitList[i].remove();
+			removedTwits += twitList[i];
+			
+		}
+	}
+	
+	console.log("== Twit List Post Search:", twitList);
+	
+}
--- assignment/assignment4/server.js
+++ assignment/assignment4/server.js
@@ -4,3 +4,16 @@
  * name:
  * email:
  */
+
+var http = require('http');
+
+function fun1 () {
+
+	console.log ("== Yeetus");
+
+}
+
+var server = http.createServer(fun1);
+server.listen(3000, function(){
+	console.log("Listening");
+});

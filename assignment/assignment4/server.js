4b126f38836666cec8ec60b995fd10f706ce4204
Lyell Read <lyellread@gmail.com>
2019-05-15 18:52:42 -0700

    Finished Assignment 4
    'twas a piece of cake ;)

--- assignment/assignment4/server.js
+++ assignment/assignment4/server.js
@@ -1,19 +1,77 @@
 /*
  * Write your server code in this file.
  *
- * name:
- * email:
+ * name:Lyell Read
+ * email:readly@oregonstate.edu
  */
 
 var http = require('http');
+var fs = require('fs');
 
-function fun1 () {
+var index_html = fs.readFileSync('./public/index.html');
+var index_js = fs.readFileSync('./public/index.js');
+var style_css = fs.readFileSync('./public/style.css');
+var html_404 = fs.readFileSync('./public/404.html');
 
-	console.log ("== Yeetus");
 
+function requestHandler (request, response) {
+
+	
+	if (request.url === '/index.html' || request.url === '/'){
+		
+		response.statusCode = 200;
+		response.setHeader('Content-Type', 'text/html');
+		
+		/*write index.html*/
+		response.write(index_html);
+		
+		/*send*/
+		response.end();
+		return;
+		
+	}
+	
+	else if (request.url === '/index.js'){
+		
+		response.statusCode = 200;
+		response.setHeader('Content-Type', 'application/javascript');
+		
+		/*write index.js*/
+		response.write(index_js);
+		
+		/*send*/
+		response.end();
+		return;
+	}
+	
+	else if (request.url === '/style.css'){
+		
+		response.statusCode = 200;
+		response.setHeader('Content-Type', 'text/css');
+		
+		/*write style.css*/
+		response.write(style_css);
+		
+		/*send*/
+		response.end();
+		return;
+	}
+	
+	else {
+		
+		response.statusCode = 404;
+		response.setHeader('Content-Type', 'text/html');
+		
+		/*write 404.html*/
+		response.write(html_404);
+		
+		/*send*/
+		response.end();
+		return;
+		
+	}
 }
 
-var server = http.createServer(fun1);
-server.listen(3000, function(){
-	console.log("Listening");
-});
+var server = http.createServer(requestHandler);
+var port = (process.env.PORT || 3000);
+server.listen(port, function(){console.log("Started; Listening on:", port);});
